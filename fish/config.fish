7f680765225e01f4af00f73ed28062d550af606e
Sean Brage <sean.brage@teliax.com>
2020-04-23 16:37:50 -0600

    auto-install fisher on new systems

--- fish/config.fish
+++ fish/config.fish
@@ -4,5 +4,11 @@ if test (uname -s) = "Darwin"
     set PATH /usr/local/bin $PATH
 end
 
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
 starship init fish | source
 
