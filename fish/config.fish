4d01e0dece5734fa270c032cdddc125b21b69590
Sean Brage <sean.brage@teliax.com>
2020-04-27 08:55:51 -0600

    adds ruby/rbenv configuration to config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,19 @@
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
+# Python + Homebrew for macOS
 if test (uname -s) = "Darwin"
     set PATH /usr/local/opt/python/libexec/bin $PATH
     set PATH /usr/local/sbin $PATH
     set PATH /usr/local/bin $PATH
 end
 
-if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
-end
+# Ruby Stuff
+set PATH ~/.rbenv/shims $PATH
+export RUBY_CONFIGURE_OPTS="--with-oppenssl-dir=(brew --prefix openssl@1.1)"
 
 starship init fish | source
 
